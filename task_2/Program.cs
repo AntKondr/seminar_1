e492fe42e54b509e9af8262ba8816b77372cbb37
Anton_Kondratev <elkondro@gmail.com>
2023-01-31 00:03:36 +0700

    task_2 solution

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Seminar_1, task_2\n");
+
+Console.Write("input number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) {
+    Console.Write($"max -> {a}");
+}
+else if (b > a) {
+    Console.Write($"max -> {b}");
+}
+else {
+    Console.Write("numbers are equal");
+}
