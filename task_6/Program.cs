ede471a955a7c6411733ca7ef2f2b18520c4264d
Anton_Kondratev <elkondro@gmail.com>
2023-01-31 00:51:08 +0700

    task_6 solution

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Seminar_1, task_6\n");
+
+Console.Write("input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 0 | a < 0) {
+    if (a % 2 == 0) {
+        Console.Write($"{a} -> is even number");
+    }
+    else {
+        Console.Write($"{a} -> is odd number");
+    }
+}
+else {
+    Console.Write("you input a zero!");
+}
